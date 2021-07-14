60b9faf58cad940eb25069e995c4409a14c04087
Saad Bazaz <saadbazaz@hotmail.com>
2021-07-14 20:47:08 +0500

    پھلے کوشش

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # UrduPython
+Write simple Python in Urdu.
+
+## How to Use
+1. Write Urdu code in ```سامپل۔پے```
+The mappings are as following:
+```
+    "چھاپ": "print",
+    "۱": "1",
+    "۲": "2",
+    "۳": "3",
+    "۴": "4",
+    "۵": "5",
+    "۶": "6",
+    "۷": "7",
+    "۸": "8",
+    "۹": "9",
+    "۰": "0",
+    "اگر": "if",
+    "ورنہ": "else"
+```
+
+2. Translate and run the code in one command: ```python urdu_python.py```
---
+++ urdu_python.py
@@ -0,0 +1,34 @@
+ur_pyfile = open("سامپل۔پے")
+
+language_dict = {
+    "چھاپ": "print",
+    "۱": "1",
+    "۲": "2",
+    "۳": "3",
+    "۴": "4",
+    "۵": "5",
+    "۶": "6",
+    "۷": "7",
+    "۸": "8",
+    "۹": "9",
+    "۰": "0",
+    "اگر": "if",
+    "ورنہ": "else"
+}
+
+words = ur_pyfile.read()
+
+for key, value in language_dict.items():
+    words = words.replace(key, value)
+
+eng_pyfile = open("eng.py", "wt")
+n = eng_pyfile.write(words)
+eng_pyfile.close()
+
+import eng
+
+import os
+if os.path.exists("eng.py"):
+  os.remove("eng.py")
+else:
+  print("The file does not exist")
