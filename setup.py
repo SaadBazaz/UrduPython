461de9f145925530e668b711f44828ef51c61c8d
Saad Bazaz <saadbazaz@hotmail.com>
2021-12-16 12:12:45 +0500

    Get install_requires from requirements.txt

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 # References:
 # - https://stackoverflow.com/questions/27494758/how-do-i-make-a-python-script-executable
 
+import os
 from setuptools import setup, find_packages
 
 files = ["samples/*/*", "languages/*/*.yaml"]
 
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='urdupython',
-    version='0.0.8',
+    version='0.0.9',
     author='Saad Bazaz',
     author_email='saadbazaz@hotmail.com',
     url='https://github.com/saadbazaz/UrduPython',
+
+    install_requires=required,
     # packages=['urdupython', 'modes', 'filters', 'languages'],
     packages=find_packages(),
     package_data = {'urdupython' : files },
